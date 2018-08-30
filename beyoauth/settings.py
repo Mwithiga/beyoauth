f66222e5c2b084a983d9845584522a4be5741fc5
Mwithiga <mwithigajames@outlook.com>
2018-08-30 14:41:52 +0300

    app pass

--- beyoauth/settings.py
+++ beyoauth/settings.py
@@ -132,4 +132,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'mwithiganjoroge@gmail.com' 
-EMAIL_HOST_PASSWORD = '5yndicate' 
+EMAIL_HOST_PASSWORD = 'dfugcybfgglqihhc' 
